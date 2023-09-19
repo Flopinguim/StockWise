41038174c65225c32e0d7369bf1fd2f8473aa311
Flopinguim <08joaovitor01@gmail.com>
2023-09-19 01:45:09 -0300

    Estruturação do projeto

---
+++ StockWise/DAL/Model/Itens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public  class Itens
+    {
+        //Declaração dos Atributos de Classe: Pessoa 
+        public int Codigo { get; set; }
+        public string Nome { get; set;}
+        public string Descricao { get; set;}
+        public DateTime DataAquisicao { get; set; }
+        public string NumeroDeSerie { get; set; }
+        public string VidaUtil { get;set; }
+        public double ValorDeCompra { get; set; }
+        public string Categoria { get; set; }
+        public string Setor { get;set; }
+        public bool Inativo { get; set; }
+
+    }
+}
